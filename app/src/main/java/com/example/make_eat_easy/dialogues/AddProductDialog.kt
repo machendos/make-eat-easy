cd6406c364a5bada571affc15d8939fc093a5629
Nikita Machekhin <nicitamac@gmail.com>
2018-05-13 00:15:51 +0300

    Create separate file with add product dialog

---
+++ app/src/main/java/com/example/make_eat_easy/dialogues/AddProductDialog.kt
@@ -0,0 +1,50 @@
+package com.example.make_eat_easy.dialogues
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import com.example.make_eat_easy.R
+import com.example.make_eat_easy.databinding.AddProductDialogBinding
+import com.example.make_eat_easy.viewmodels.ProductViewModel
+
+class AddProductDialog(private val context: Context, val viewModel: ProductViewModel) {
+
+    private val bindingDialog: AddProductDialogBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(context),
+        R.layout.add_product_dialog, null, false
+    )
+
+    private val dialog = AlertDialog.Builder(context)
+        .setView(bindingDialog.root)
+        .setTitle("Add new product")
+        .setPositiveButton("Add") { _, _ ->
+
+            viewModel.addProduct(
+                bindingDialog.productName.text.toString(),
+                bindingDialog.measureName.text.toString(),
+                bindingDialog.categoryName.text.toString()
+            )
+
+        }.setNegativeButton("Cancel") { _, _ -> }
+
+    init {
+        bindingDialog.categoryName.setAdapter(ArrayAdapter<String>(
+            context,
+            R.layout.element_autocomplete,
+            R.id.autocomplete_element,
+            viewModel.productRepository.categories.value!!.map { it.categoryName }
+        ))
+
+        bindingDialog.measureName.setAdapter(ArrayAdapter<String>(
+            context,
+            R.layout.element_autocomplete,
+            R.id.autocomplete_element,
+            viewModel.productRepository.measures.value!!.map { it.measureName }
+        ))
+
+    }
+
+    fun show() = dialog.show()
+}
