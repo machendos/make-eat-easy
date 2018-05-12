8ca970af57df2a0f02620c3c947ddd162298e392
Nikita Machekhin <nicitamac@gmail.com>
2018-05-12 23:04:12 +0300

    Replace helper with separate

--- app/src/main/java/com/example/make_eat_easy/views/Products.kt
+++ app/src/main/java/com/example/make_eat_easy/views/Products.kt
@@ -11,13 +11,13 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.make_eat_easy.R
 import com.example.make_eat_easy.adapters.ProductsAdapter
 import com.example.make_eat_easy.databinding.AddCategoryDialogBinding
 import com.example.make_eat_easy.databinding.AddProductDialogBinding
 import com.example.make_eat_easy.databinding.ProductEditDialogBinding
 import com.example.make_eat_easy.databinding.ProductsBinding
+import com.example.make_eat_easy.util.ProductTouchHelper
 import com.example.make_eat_easy.viewmodels.ProductViewModel
 
 class Products : AppCompatActivity() {
@@ -141,76 +141,9 @@ class Products : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = productAdapter
 
-        ItemTouchHelper(object :
-            ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0) {
+        ItemTouchHelper(ProductTouchHelper(viewModel, productAdapter)
 
-            override fun getMovementFlags(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder
-            ): Int {
-                val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
-                val swipeFlags = ItemTouchHelper.START or ItemTouchHelper.END
-                return if (viewHolder.itemViewType == ProductsAdapter.CATEGORY) 0 else ItemTouchHelper.Callback.makeMovementFlags(
-                    dragFlags,
-                    swipeFlags
-                )
-            }
-
-            override fun isLongPressDragEnabled(): Boolean {
-                return true
-            }
-
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                val targetPosition = target.adapterPosition
-                if (targetPosition == 0) return false
-                val draggedPosition = viewHolder.adapterPosition
-
-                val list = viewModel.productsCategoryList.value!!
-
-                val draggedElement = list[draggedPosition]
-                val targetElement = list[targetPosition]
-
-                val draggedOrder = draggedElement.order
-                draggedElement.order = targetElement.order
-                targetElement.order = draggedOrder
-
-                list[draggedPosition] = targetElement
-                list[targetPosition] = draggedElement
-
-                productAdapter.notifyItemMoved(draggedPosition, targetPosition)
-                return false
-            }
-
-            override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
-                if (actionState == ItemTouchHelper.ACTION_STATE_IDLE) {
-                    var currCaregoryId: Int? = null
-                    viewModel.productsCategoryList.value!!.forEach {
-                        if (it.isProduct) it.categoryId = currCaregoryId
-                        else currCaregoryId = it.categoryId
-                    }
-
-                    viewModel.productRepository.updateProductOrders(viewModel.productsCategoryList.value!!)
-                }
-                super.onSelectedChanged(viewHolder, actionState)
-            }
-
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-
-                val id = viewModel
-                    .productsCategoryList
-                    .value!![viewHolder.adapterPosition]
-                    .productId
-
-                viewModel.productRepository.deleteProduct(id)
-
-            }
-
-        }).attachToRecyclerView(recyclerView)
+        ).attachToRecyclerView(recyclerView)
 
     }
 }
