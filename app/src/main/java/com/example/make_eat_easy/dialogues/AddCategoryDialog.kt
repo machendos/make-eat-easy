d641a713f811ed51891bd8245bf771d2963e0425
Nikita Machekhin <nicitamac@gmail.com>
2018-05-12 23:53:25 +0300

    Create separate file with add category dialog

---
+++ app/src/main/java/com/example/make_eat_easy/dialogues/AddCategoryDialog.kt
@@ -0,0 +1,31 @@
+package com.example.make_eat_easy.dialogues
+
+import android.content.Context
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import com.example.make_eat_easy.R
+import com.example.make_eat_easy.databinding.AddCategoryDialogBinding
+import com.example.make_eat_easy.viewmodels.ProductViewModel
+
+class AddCategoryDialog(
+    private val context: Context, val viewModel: ProductViewModel
+) {
+
+    private val bindingDialog: AddCategoryDialogBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(context),
+        R.layout.add_category_dialog, null, false
+    )
+
+    private val dialog = AlertDialog.Builder(context)
+        .setView(bindingDialog.root)
+        .setTitle("Add new category")
+        .setPositiveButton("Add") { _, _ ->
+
+            viewModel.addCategory(bindingDialog.addCategoryName.text.toString())
+
+        }.setNegativeButton("Cancel") { _, _ -> }
+
+    fun show() = dialog.show()
+
+}
