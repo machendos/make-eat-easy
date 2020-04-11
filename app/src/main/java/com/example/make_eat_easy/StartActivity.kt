0f3ed4a533ac1fec66dd07a95134d50ebed4c6bf
Nikita Machekhin <nicitamac@gmail.com>
2020-04-11 16:41:12 +0300

    Add sign-in with google service

--- app/src/main/java/com/example/make_eat_easy/StartActivity.kt
+++ app/src/main/java/com/example/make_eat_easy/StartActivity.kt
@@ -1,6 +1,9 @@
 package com.example.make_eat_easy
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
@@ -15,8 +18,19 @@ class StartActivity: AppCompatActivity() {
 
         if (FirebaseAuth.getInstance().currentUser == null) {
             startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), LOGIN_CODE)
+        } else {
+            Toast.makeText(this, "Already signin", Toast.LENGTH_LONG).show()
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == LOGIN_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "Signin OK", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
