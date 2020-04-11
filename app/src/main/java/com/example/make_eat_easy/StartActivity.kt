c3634a1727d680230f165cea1194a32a3332d45d
Nikita Machekhin <nicitamac@gmail.com>
2020-04-11 14:14:49 +0300

    Add basic StartActivity class

---
+++ app/src/main/java/com/example/make_eat_easy/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.make_eat_easy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_activity)
+    }
+}
