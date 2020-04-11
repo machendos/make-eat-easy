c78906256efed0cc3e215d90d6711df6c65bb22e
Nikita Machekhin <nicitamac@gmail.com>
2020-04-11 14:21:45 +0300

    Add sign-in with firebaseUI

--- app/src/main/java/com/example/make_eat_easy/StartActivity.kt
+++ app/src/main/java/com/example/make_eat_easy/StartActivity.kt
@@ -2,11 +2,21 @@ package com.example.make_eat_easy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
 class StartActivity: AppCompatActivity() {
+
+    private val LOGIN_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_activity)
+
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), LOGIN_CODE)
+        }
+
     }
+
 }
