176f91359aeb77762314c97c3cba7ba717bc7a33
Nikita Machekhin <nicitamac@gmail.com>
2020-04-12 05:59:21 +0300

    Add empty activity for Firebase interact

---
+++ app/src/main/java/com/example/make_eat_easy/FirebaseInteract.kt
@@ -0,0 +1,12 @@
+package com.example.make_eat_easy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FirebaseInteract : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firebase_interact)
+    }
+}
