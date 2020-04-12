607de65b836ae83c03fe5f52a2d65bd5f43eae8d
Nikita Machekhin <nicitamac@gmail.com>
2020-04-12 05:43:51 +0300

    Add on password reset listener

--- app/src/main/java/com/example/make_eat_easy/StartActivity.kt
+++ app/src/main/java/com/example/make_eat_easy/StartActivity.kt
@@ -22,14 +22,20 @@ class StartActivity: AppCompatActivity() {
             Toast.makeText(this, "Already signin", Toast.LENGTH_LONG).show()
         }
 
+        Toast.makeText(this, auth.languageCode, Toast.LENGTH_LONG).show()
+
         findViewById<Button>(R.id.register_button).setOnClickListener { signup(
             findViewById<EditText>(R.id.email_register).text.toString(),
             findViewById<EditText>(R.id.pass_register).text.toString()
         ) }
 
-        findViewById<Button>(R.id.login_button).setOnClickListener { signup(
+        findViewById<Button>(R.id.login_button).setOnClickListener { signin(
             findViewById<EditText>(R.id.email_login).text.toString(),
             findViewById<EditText>(R.id.pass_login).text.toString()
+        ) }
+
+        findViewById<Button>(R.id.pass_reset_button).setOnClickListener { resetPass(
+            findViewById<EditText>(R.id.email_pass_reset).text.toString()
         ) }
 
 
@@ -38,21 +44,34 @@ class StartActivity: AppCompatActivity() {
     fun signup(email: String, pass: String) {
         Log.d("REGISTER", "$email $pass")
         auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
-            Toast.makeText(this, "Create new user", Toast.LENGTH_LONG).show()
-            signin(email, pass)
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Create new user", Toast.LENGTH_LONG).show()
+                signin(email, pass)
+            } else {
+                Toast.makeText(this, "error: Create new user:: ${it.exception?.localizedMessage}", Toast.LENGTH_LONG).show()
+            }
         }.addOnFailureListener {
-            Toast.makeText(this, "FAIL: Create new user", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "FAIL: Create new user:: ${it.message}", Toast.LENGTH_LONG).show()
         }
     }
 
     fun signin(email: String, pass: String) {
         Log.d("LOGIN", "$email $pass")
         auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
-            Toast.makeText(this, "Authorised", Toast.LENGTH_LONG).show()
+            if (it.isSuccessful) Toast.makeText(this, "Authorised", Toast.LENGTH_LONG).show()
+            else Toast.makeText(this, "error: Authorised:: ${it.exception?.message}", Toast.LENGTH_LONG).show()
         }.addOnFailureListener {
-            Toast.makeText(this, "FAIL: Authorised", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "FAIL: Authorised ${it.message}", Toast.LENGTH_LONG).show()
         }
     }
 
+    fun resetPass(email: String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("asd", "Email sent.")
+                } else Toast.makeText(this, "FAIL: ${task.exception?.message}", Toast.LENGTH_LONG).show()
+            }
+    }
 
 }
