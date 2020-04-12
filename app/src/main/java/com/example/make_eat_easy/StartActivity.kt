fcb6ec321cec0933427704bbf6484f86b1148be9
Nikita Machekhin <nicitamac@gmail.com>
2020-04-12 05:13:14 +0300

    Add own registration and signin

--- app/src/main/java/com/example/make_eat_easy/StartActivity.kt
+++ app/src/main/java/com/example/make_eat_easy/StartActivity.kt
@@ -1,36 +1,58 @@
 package com.example.make_eat_easy
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 
 class StartActivity: AppCompatActivity() {
 
-    private val LOGIN_CODE = 1
+    val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_activity)
 
         if (FirebaseAuth.getInstance().currentUser == null) {
-            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), LOGIN_CODE)
+            Toast.makeText(this, "Please authorise", Toast.LENGTH_LONG).show()
         } else {
             Toast.makeText(this, "Already signin", Toast.LENGTH_LONG).show()
         }
 
+        findViewById<Button>(R.id.register_button).setOnClickListener { signup(
+            findViewById<EditText>(R.id.email_register).text.toString(),
+            findViewById<EditText>(R.id.pass_register).text.toString()
+        ) }
+
+        findViewById<Button>(R.id.login_button).setOnClickListener { signup(
+            findViewById<EditText>(R.id.email_login).text.toString(),
+            findViewById<EditText>(R.id.pass_login).text.toString()
+        ) }
+
+
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == LOGIN_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                Toast.makeText(this, "Signin OK", Toast.LENGTH_LONG).show()
-            }
+    fun signup(email: String, pass: String) {
+        Log.d("REGISTER", "$email $pass")
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+            Toast.makeText(this, "Create new user", Toast.LENGTH_LONG).show()
+            signin(email, pass)
+        }.addOnFailureListener {
+            Toast.makeText(this, "FAIL: Create new user", Toast.LENGTH_LONG).show()
         }
     }
+
+    fun signin(email: String, pass: String) {
+        Log.d("LOGIN", "$email $pass")
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+            Toast.makeText(this, "Authorised", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, "FAIL: Authorised", Toast.LENGTH_LONG).show()
+        }
+    }
+
 
 }
