f38273241d03815a1b5bfedb46940fa59c1cd347
Nikita Machekhin <nicitamac@gmail.com>
2020-04-19 14:24:31 +0300

    Refactor SignIn

--- app/src/main/java/com/example/make_eat_easy/SignIn.kt
+++ app/src/main/java/com/example/make_eat_easy/SignIn.kt
@@ -11,36 +11,15 @@ import com.google.android.material.snackbar.Snackbar
 
 class SignIn: AppCompatActivity() {
 
-//    lateinit var binding: SigninBinding
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        supportActionBar?.setDisplayShowTitleEnabled(false)
-//
-//
-//        val vm = ViewModelProvider(this)[SignInViewModel::class.java]
-//        binding = DataBindingUtil.setContentView(this, R.layout.signin_activity)
-//        binding.signInViewModel = SignInViewModel()
-//
-//        vm.genericEventSample.observe(this) {
-//            Log.d("asdasd", "ну тут и так понятно бляaaaaaaaaaaaaaaaaaaaaaaaaaa")
-//            Snackbar
-//                .make(binding.root, "Generic One Time Event", Snackbar.LENGTH_LONG)
-//                .show()
-//        }
-//    }
-
     private lateinit var viewModel: SignInViewModel
     private lateinit var binding: SigninActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.signin_activity)
         viewModel = ViewModelProvider(this)[SignInViewModel::class.java]
         binding.viewModel = this.viewModel
-
-
 
         viewModel.genericEventSample.observe(this) {
             Snackbar
