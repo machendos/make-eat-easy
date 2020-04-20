e1779084562c1d727d5ce113c9b95d367ab3e475
Nikita Machekhin <nicitamac@gmail.com>
2020-04-20 14:12:52 +0300

    Implement enter to app by signup

--- app/src/main/java/com/example/make_eat_easy/SignUp.kt
+++ app/src/main/java/com/example/make_eat_easy/SignUp.kt
@@ -1,13 +1,39 @@
 package com.example.make_eat_easy
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import com.example.make_eat_easy.databinding.SignupActivityBinding
+import com.example.make_eat_easy.viewmodels.SignUpViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class SignUp: AppCompatActivity() {
+
+    private lateinit var viewModel: SignUpViewModel
+    private lateinit var binding: SignupActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.signup)
 
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+        binding = DataBindingUtil.setContentView(this, R.layout.signup_activity)
+        viewModel = ViewModelProvider(this)[SignUpViewModel::class.java]
+        binding.viewModel = this.viewModel
+
+        viewModel.success.observe(this) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        viewModel.error.observe(this) {
+            Snackbar
+                .make(binding.root, it, Snackbar.LENGTH_LONG)
+                .show()
+        }
+
+
     }
 }
+
