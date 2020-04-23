a511ec99b6912105b823eae8eb4e77e2fb05b060
Nikita Machekhin <nicitamac@gmail.com>
2020-04-23 17:25:32 +0300

    Create simple vm for start activity

---
+++ app/src/main/java/com/example/make_eat_easy/viewmodels/StartViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.make_eat_easy.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.make_eat_easy.Authenticator
+
+class StartViewModel : ViewModel() {
+
+    private val authenticator = Authenticator()
+
+    val alreadyLoggedIn = authenticator.alreadyLoggedIn()
+
+}
