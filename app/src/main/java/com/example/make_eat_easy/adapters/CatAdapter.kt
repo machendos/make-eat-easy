bfee5fad4223eda831bab185e3ec4dd9be5bd76b
Nikita Machekhin <nicitamac@gmail.com>
2020-04-25 22:15:41 +0300

    Add deleteProduct function to adapter

--- app/src/main/java/com/example/make_eat_easy/adapters/CatAdapter.kt
+++ app/src/main/java/com/example/make_eat_easy/adapters/CatAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.make_eat_easy.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,7 +8,9 @@ import android.widget.TextView
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.example.make_eat_easy.R
+import com.example.make_eat_easy.firebase.Authenticator
 import com.example.make_eat_easy.models.Product
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class NewAdapter(val products: MutableLiveData<MutableList<Product>>):
@@ -45,4 +48,23 @@ class NewAdapter(val products: MutableLiveData<MutableList<Product>>):
     }
 
     override fun getItemCount() = products.value!!.size
+
+    fun deleteProduct(position: Int) {
+        val productId = products.value!![position].productId
+        products.value!!.removeAt(position)
+        notifyDataSetChanged()
+
+        FirebaseFirestore
+            .getInstance()
+            .collection("userData")
+            .document(Authenticator().getEmail())
+            .collection("product").document(productId + "asd").delete().addOnFailureListener {
+                Log.d("debuggg", it.message)
+            }.addOnSuccessListener {
+                Log.d("debuggg", "REAT")
+            }
+
+    }
+
+
 }
