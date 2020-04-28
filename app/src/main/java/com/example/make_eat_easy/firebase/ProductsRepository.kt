3f512f740f83ac3ffca8a8afc26f7fe102e799d1
Nikita Machekhin <nicitamac@gmail.com>
2020-04-28 17:24:52 +0300

    Add measures listener

--- app/src/main/java/com/example/make_eat_easy/firebase/ProductsRepository.kt
+++ app/src/main/java/com/example/make_eat_easy/firebase/ProductsRepository.kt
@@ -1,6 +1,7 @@
 package com.example.make_eat_easy.firebase
 
 import androidx.lifecycle.MutableLiveData
+import com.example.make_eat_easy.models.Measure
 import com.example.make_eat_easy.models.Product
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
@@ -8,6 +9,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 class ProductsRepository {
 
     var products: MutableLiveData<MutableList<Product>> = MutableLiveData(mutableListOf())
+    var measures: MutableLiveData<MutableList<Measure>> = MutableLiveData(mutableListOf())
 
     private val db = FirebaseFirestore.getInstance()
 
@@ -23,6 +25,40 @@ class ProductsRepository {
 
 
     init {
+
+
+        measureCategory.addSnapshotListener { snapshot, _ ->
+
+            snapshot!!.documentChanges.forEach { documentChange ->
+                val document = documentChange.document
+                val measureId = document.get("measureId") as Int
+                val measureName = document.get("measureName") as String
+                val parentMeasureId = document.get("parentMeasureId") as Int?
+                val parentMeasureFactor = document.get("parentMeasureFactor") as Float
+
+                when (documentChange.type) {
+
+                    DocumentChange.Type.ADDED ->
+                        measures.value!!.add(Measure(measureId, measureName, parentMeasureId, parentMeasureFactor))
+
+                    DocumentChange.Type.MODIFIED -> {
+                        val element = measures.value!!.find { it.measureId == measureId }
+                        element?.measureId = measureId
+                        element?.measureName = measureName
+                        element?.parentMeasureId = parentMeasureId
+                        element?.parentMeasureFactor = parentMeasureFactor
+                    }
+
+                    DocumentChange.Type.REMOVED -> {
+                        val index = measures.value!!.indexOfFirst { it.measureId == measureId }
+                        measures.value!!.removeAt(index)
+                    }
+
+                }
+                measures.postValue(measures.value)
+            }
+        }
+
         productCategory.addSnapshotListener { snapshot, _ ->
 
             snapshot!!.documentChanges.forEach { documentChange ->
