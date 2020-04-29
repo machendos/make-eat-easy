32380218b93b6bdc7220333047ed375eb7d76082
Nikita Machekhin <nicitamac@gmail.com>
2020-04-29 16:32:12 +0300

    Add link to product repository in vm

--- app/src/main/java/com/example/make_eat_easy/viewmodels/ProductViewModel.kt
+++ app/src/main/java/com/example/make_eat_easy/viewmodels/ProductViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.make_eat_easy.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.make_eat_easy.firebase.ProductsRepository
 
 class ProductViewModel: ViewModel() {
+
+    val productRepository = ProductsRepository()
 
 }
