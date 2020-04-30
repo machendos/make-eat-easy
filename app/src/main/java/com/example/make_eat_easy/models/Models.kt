a5f62759552b50b90e30fcb1082cd891f62b420b
Nikita Machekhin <nicitamac@gmail.com>
2020-04-30 17:00:32 +0300

    Add orders to models

--- app/src/main/java/com/example/make_eat_easy/models/Models.kt
+++ app/src/main/java/com/example/make_eat_easy/models/Models.kt
@@ -4,12 +4,17 @@ class Product(
     productId: Int = 0,
     productName: String = "",
     measureId: Int = 0,
-    categoryId: Int = 0
+    categoryId: Int = 0,
+    order: Int = 0
 ) {
     val productId = productId
     var productName = productName
     var measureId = measureId
-    var categoryId =categoryId
+    var categoryId = categoryId
+    var order = order
+    override fun toString(): String {
+        return "Product(productId=$productId, productName='$productName', measureId=$measureId, categoryId=$categoryId, order=$order)"
+    }
 }
 
 class Measure(
@@ -22,12 +27,15 @@ class Measure(
     var measureName = measureName
     var parentMeasureId = parentMeasureId
     var parentMeasureFactor = parentMeasureFactor
+    override fun toString(): String {
+        return "Measure(measureId=$measureId, measureName='$measureName', parentMeasureId=$parentMeasureId, parentMeasureFactor=$parentMeasureFactor)"
+    }
 }
 
 class Category(
     categoryId: Int = 0,
     categoryName: String = "",
-//    order: Int = 0
+    order: Int = 0
 
 ) {
 
@@ -35,7 +43,10 @@ class Category(
 
     var categoryName = categoryName
 
-//    val order = order
+    var order = order
+    override fun toString(): String {
+        return "Category(categoryId=$categoryId, categoryName='$categoryName', order=$order)"
+    }
 
 }
 
@@ -51,18 +62,26 @@ class CategoryProduct() {
 
     var categoryId: Int = 0
 
+    var order: Int = 0
+
     constructor(category: Category) : this() {
         isProduct = false
 
         categoryId = category.categoryId
         categoryName = category.categoryName
+        order = category.order
     }
 
     constructor(product: Product) : this() {
-        var productId = product.productId
-        var productName = product.productName
-        var measureId = product.measureId
-        var categoryId = product.categoryId
+        productId = product.productId
+        productName = product.productName
+        measureId = product.measureId
+        categoryId = product.categoryId
+        order = product.order
+    }
+
+    override fun toString(): String {
+        return "CategoryProduct(isProduct=$isProduct, productId=$productId, productName='$productName', measureId=$measureId, categoryName='$categoryName', categoryId=$categoryId, order=$order)"
     }
 
 
