a70b6cb2c47dc576ea2fe60f2bfdc74771bf9a62
Nikita Machekhin <nicitamac@gmail.com>
2020-05-01 13:55:12 +0300

    Update adapters for multiple types

--- app/src/main/java/com/example/make_eat_easy/adapters/CatAdapter.kt
+++ app/src/main/java/com/example/make_eat_easy/adapters/CatAdapter.kt
@@ -1,65 +1,84 @@
 package com.example.make_eat_easy.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.example.make_eat_easy.R
 import com.example.make_eat_easy.firebase.Authenticator
+import com.example.make_eat_easy.models.CategoryProduct
 import com.example.make_eat_easy.models.Measure
-import com.example.make_eat_easy.models.Product
 import com.google.firebase.firestore.FirebaseFirestore
 
 
 class NewAdapter(
-    val products: MutableLiveData<MutableList<Product>>,
+    val productsCategories: MediatorLiveData<MutableList<CategoryProduct>>,
     val measures: MutableLiveData<MutableList<Measure>>
-):
-    RecyclerView.Adapter<NewAdapter.ProductHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-//    class CategoryHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//
-//    }
+    val CATEGORY = 1
+    val PRODUCT = 2
 
-    class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val productNameView = itemView.findViewById<TextView>(R.id.product_name)
-        val categoryNameView = itemView.findViewById<TextView>(R.id.category_name)
-        val measureNameView = itemView.findViewById<TextView>(R.id.measure_name)
+    class CategoryHolder(categoryView: View) : RecyclerView.ViewHolder(categoryView) {
+        val categoryName = categoryView.findViewById<TextView>(R.id.category_text)
     }
 
-    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
-
-        val product = products.value!![position]
-        val measureId = product.measureId
-        val measure = measures.value!!.find { it.measureId == measureId }
-
-        holder.productNameView.text = products.value!![position].productName
-        holder.categoryNameView.text = products.value!![position].categoryId.toString()
-        holder.measureNameView.text = measure!!.measureName
+    class ProductHolder(productView: View) : RecyclerView.ViewHolder(productView) {
+        val productNameView = productView.findViewById<TextView>(R.id.product_name)
+        val measureNameView = productView.findViewById<TextView>(R.id.measure_name)
     }
 
-//    override fun getItemViewType(position: Int): Int {
-//        return if (TextUtils.isEmpty(employees.get(position).getEmail())) {
-////            TYPE_CALL
-//        } else {
-////            TYPE_EMAIL
-//        }
-//    }
 
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): NewAdapter.ProductHolder {
-        val textView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.product_item, parent, false)
-        return ProductHolder(textView)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        Log.d("asdasd<-RECYCLER", "bind")
+        Log.d("asdasd", productsCategories.value!!.joinToString(transform = {product -> product.toString() }))
+
+        val item = productsCategories.value!![position]
+
+        if (getItemViewType(position) == PRODUCT) {
+            Log.d("asdasd<-RECYCLER", "ПРОДУКТ")
+            val measureId = item.measureId
+            val measure = measures.value!!.find { it.measureId == measureId }
+            (holder as ProductHolder).productNameView.text = item.productName
+            Log.d("asdasd<-RECYCLER-ИМЯ", item.productName)
+
+//            TODO:
+            holder.measureNameView.text = measure?.measureName ?: "gg"
+        } else {
+            Log.d("asdasd<-RECYCLER", "НЕ ПРОДУКТ")
+            (holder as CategoryHolder).categoryName.text = item.categoryName
+        }
     }
 
-    override fun getItemCount() = products.value!!.size
+    override fun getItemViewType(position: Int): Int =
+        if (productsCategories.value!![position].isProduct) PRODUCT else CATEGORY
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerView.ViewHolder {
+        return if (viewType == PRODUCT) {
+            val productView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.product_item, parent, false)
+            NewAdapter.ProductHolder(productView)
+        } else {
+            val categoryView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.category_item, parent, false)
+            NewAdapter.CategoryHolder(categoryView)
+        }
+    }
+
+    override fun getItemCount() = productsCategories.value!!.size
 
     fun deleteProduct(position: Int) {
 
-        val productId = products.value!![position].productId
+        val productId = productsCategories.value!![position].productId
 
         FirebaseFirestore
             .getInstance()
