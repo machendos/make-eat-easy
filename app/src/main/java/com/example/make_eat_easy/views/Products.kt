73ef4db0451584128d5b470b631f96ddc749802c
Nikita Machekhin <nicitamac@gmail.com>
2020-05-04 13:04:12 +0300

    Add drag&drop

--- app/src/main/java/com/example/make_eat_easy/views/Products.kt
+++ app/src/main/java/com/example/make_eat_easy/views/Products.kt
@@ -1,15 +1,19 @@
 package com.example.make_eat_easy.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.make_eat_easy.R
 import com.example.make_eat_easy.adapters.NewAdapter
 import com.example.make_eat_easy.databinding.ProductsBinding
 import com.example.make_eat_easy.viewmodels.ProductViewModel
+
 
 class Products : AppCompatActivity() {
 
@@ -36,5 +40,55 @@ class Products : AppCompatActivity() {
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = productAdapter
+
+
+
+        ItemTouchHelper(object: ItemTouchHelper.SimpleCallback(0, 0) {
+
+
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+                val swipeFlags = ItemTouchHelper.START or ItemTouchHelper.END
+                return if (viewHolder.itemViewType == NewAdapter.CATEGORY) 0 else ItemTouchHelper.Callback.makeMovementFlags(
+                    dragFlags,
+                    swipeFlags
+                )
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                val id = viewModel.productsCategoryList.value!![viewHolder.adapterPosition].productId
+
+                Log.d("asd", id.toString())
+
+                viewModel.productRepository.deleteProduct(id)
+            }
+
+            override fun getSwipeDirs(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+
+                if (viewHolder is NewAdapter.CategoryHolder) {
+                    Log.d("asd", "is")
+                } else Log.d("asd", "not is")
+
+                if (viewHolder is NewAdapter.CategoryHolder) return 0
+                return super.getSwipeDirs(recyclerView, viewHolder)
+            }
+
+        }).attachToRecyclerView(recyclerView)
+
     }
 }
