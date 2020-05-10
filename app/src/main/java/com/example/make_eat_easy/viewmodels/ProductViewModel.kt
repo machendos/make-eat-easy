9365dfe63c77af1571c44689503196926dc58553
Nikita Machekhin <nicitamac@gmail.com>
2020-05-10 11:23:06 +0300

    Add method for adding products in vm

--- app/src/main/java/com/example/make_eat_easy/viewmodels/ProductViewModel.kt
+++ app/src/main/java/com/example/make_eat_easy/viewmodels/ProductViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.make_eat_easy.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import com.example.make_eat_easy.firebase.ProductsRepository
@@ -74,6 +75,48 @@ class ProductViewModel : ViewModel() {
 
             productsCategoryList.value = productsCategoryList.value
         }
+    }
+
+    fun addProduct(productName: String, measureName: String, categoryName: String) {
+
+//        var maxId = 0
+//        productRepository.products.value!!
+//            .forEach { if (it.productId >= maxId) maxId = it.productId + 1 }
+
+        val measure =
+            productRepository.measures.value!!.find { it.measureName == measureName }
+        val measureId = measure?.measureId ?: productRepository.addMeasure(measureName)
+
+        if (categoryName == "") {
+            val order = productsCategoryList.value!![productsCategoryList.value!!.lastIndex].order + 1
+            productRepository.addProduct(productName, null, measureId, order)
+            return
+        }
+
+        val category =
+            productRepository.categories.value!!.find { it.categoryName == categoryName }
+
+        if (category == null) {
+            val (categoryId, categoryOrder) = productRepository.addCategory(categoryName)
+            productRepository.addProduct(productName, categoryId, measureId, categoryOrder + 1)
+
+        } else {
+            val elementsForIncrement = mutableListOf<CategoryProduct>()
+            var currOrder = category.order + 1
+            productsCategoryList.value!!.forEach { if (it.order == currOrder) {
+                Log.d("asdasd->>index", it.productName)
+                currOrder = it.order + 1
+                elementsForIncrement.add(it)
+            } }
+            productRepository.incrementOrder(elementsForIncrement)
+            productRepository.addProduct(productName, category.categoryId, measureId, category.order + 1)
+
+
+        }
+
+
+
+//        productRepository.add
     }
 
 }
