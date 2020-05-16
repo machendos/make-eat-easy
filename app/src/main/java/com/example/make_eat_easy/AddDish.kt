a55e7ff571b2b3ca18bf75499145a42b273dfb3e
Nikita Machekhin <nicitamac@gmail.com>
2020-05-16 23:11:39 +0300

    Add databinding to add dish

--- app/src/main/java/com/example/make_eat_easy/AddDish.kt
+++ app/src/main/java/com/example/make_eat_easy/AddDish.kt
@@ -4,22 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.make_eat_easy.adapters.NecessaryProductsAdapter
+import com.example.make_eat_easy.databinding.AddDishBinding
+import com.example.make_eat_easy.viewmodels.DishesViewModel
 
 
 class AddDish : Fragment() {
+
+    private lateinit var viewModel: DishesViewModel
+    private  lateinit var binding: AddDishBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
 
+        binding = DataBindingUtil.inflate<AddDishBinding>(inflater, R.layout.add_dish, container, false)
+        val view = binding.root
 
-        return inflater.inflate(R.layout.add_dish, container, false)
+        viewModel = ViewModelProvider(this).get(DishesViewModel::class.java)
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
