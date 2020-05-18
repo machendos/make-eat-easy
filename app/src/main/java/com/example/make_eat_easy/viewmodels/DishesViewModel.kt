53c32aadc72bcc64c75291d725a34fbcfa7067c6
Nikita Machekhin <nicitamac@gmail.com>
2020-05-18 08:49:36 +0300

    Finish add dish method in vm

--- app/src/main/java/com/example/make_eat_easy/viewmodels/DishesViewModel.kt
+++ app/src/main/java/com/example/make_eat_easy/viewmodels/DishesViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.make_eat_easy.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import com.example.make_eat_easy.models.CategoryDish
@@ -8,60 +9,91 @@ import com.example.make_eat_easy.repository.ProductsRepository
 
 class DishesViewModel : ViewModel() {
 
-    val dishesRepository = DishesRepository()
-    val productRepository = ProductsRepository()
+    val dishesRepository = DishesRepository
+    val productRepository = ProductsRepository
 
     val dishesCategoryList = MediatorLiveData<MutableList<CategoryDish>>()
 
     init {
 
-        dishesCategoryList.value = mutableListOf()
+        dishesCategoryList.value =
+            dishesRepository.categories.value!!.map { CategoryDish(it) }.toMutableList()
+        dishesRepository.dishes.value!!.forEach { dishesCategoryList.value!!.add(CategoryDish(it)) }
 
         fun onChangeDishCategory() {
             dishesCategoryList.value =
-                dishesRepository.categories.value!!.map {
-                    category -> CategoryDish(category)
+                dishesRepository.categories.value!!.map { category ->
+                    CategoryDish(category)
                 } as MutableList
 
-            val convertedDishes = dishesRepository.dishes.value!!.map { CategoryDish(it) } as MutableList
+            val convertedDishes =
+                dishesRepository.dishes.value!!.map { CategoryDish(it) } as MutableList
 
             dishesCategoryList.value!!.addAll(convertedDishes)
             dishesCategoryList.value!!.sortBy { it.order }
             dishesCategoryList.value = dishesCategoryList.value
+            Log.d("asd-change", dishesCategoryList.value.toString())
         }
 
         dishesCategoryList.addSource(dishesRepository.categories) { onChangeDishCategory() }
         dishesCategoryList.addSource(dishesRepository.dishes) { onChangeDishCategory() }
     }
 
-    fun addDish(dishName: String, categoryName: String?, cookDuration: String,  products: Map<String, String>) {
+    fun addDish(
+        dishName: String,
+        categoryName: String,
+        cookDuration: String,
+        products: Map<String, String>
+    ) {
+
+        Log.d("asd-before-2", dishesCategoryList.value.toString())
 
 
         var duration = 0
-        try { duration = cookDuration.toInt() } catch (err: Error) { }
+        try {
+            duration = cookDuration.toInt()
+        } catch (err: Error) {
+        }
 
         val filtredProducts = products.map { (productName, count) ->
-            val product = productRepository.products.value!!.find { it.productName ==  productName}
-            if (product == null) {
-//                TODO: ADD NEW PRODUCT
-//                val productId = productRepository.addProduct(productName, null, 1, null)
-                1 to 2.2
-            } else {
-                product.productId to count.toDouble()
+
+            var parsedCount = 0.0
+            try {
+                parsedCount = count.toDouble()
+            } catch (e: Error) {
             }
+
+            val product = productRepository.products.value!!.find { it.productName == productName }
+
+            if (product == null) {
+                val productId = productRepository
+                    .addProduct(productName, null, 1, null).toString()
+                productId to parsedCount
+            } else {
+                product.productId.toString() to count.toDouble()
+            }
+
         }.toMap().toMutableMap()
 
         if (categoryName == "") {
+            Log.d("asd-before", dishesCategoryList.value.toString())
             val order = dishesCategoryList.value!![dishesCategoryList.value!!.lastIndex].order + 1
             dishesRepository.addDish(dishName, null, duration, filtredProducts, order)
             return
         }
 
-        val category = dishesRepository.categories.value!!.find { it.categoryName == categoryName }
+        val category =
+            dishesRepository.categories.value!!.find { it.categoryName == categoryName }
 
         if (category == null) {
-//            Add category
-//            Add dish
+            val (categoryId, categoryOrder) = dishesRepository.addCategory(categoryName)
+            dishesRepository.addDish(
+                dishName,
+                categoryId,
+                duration,
+                filtredProducts,
+                categoryOrder + 1
+            )
         } else {
             val elementsForIncrement = mutableListOf<CategoryDish>()
             var currOrder = category.order + 1
@@ -71,8 +103,15 @@ class DishesViewModel : ViewModel() {
                     elementsForIncrement.add(it)
                 }
             }
-//            increment order
-//            add product
+            dishesRepository.incrementOrder(elementsForIncrement)
+            dishesRepository.addDish(
+                dishName,
+                category.categoryId,
+                duration,
+                filtredProducts,
+                category.order + 1
+
+            )
 
         }
 
