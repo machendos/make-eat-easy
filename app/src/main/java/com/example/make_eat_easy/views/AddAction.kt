e387ecfe959161157c03f79cafd3ce7a619f0773
Nikita Machekhin <nicitamac@gmail.com>
2020-05-20 21:54:26 +0300

    Add date&time pickers for add actions

--- app/src/main/java/com/example/make_eat_easy/views/AddAction.kt
+++ app/src/main/java/com/example/make_eat_easy/views/AddAction.kt
@@ -1,25 +1,54 @@
 package com.example.make_eat_easy.views
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.make_eat_easy.R
 import com.example.make_eat_easy.databinding.FragmentAddActionBinding
+import com.example.make_eat_easy.viewmodels.AddAcionViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddAction : Fragment() {
 
     private lateinit var binding: FragmentAddActionBinding
+    private lateinit var viewModel: AddAcionViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_action, container, false)
         val view = binding.root
+
+
+        viewModel = ViewModelProvider(this)[AddAcionViewModel::class.java]
+
+        binding.cookingRadio.setOnClickListener {
+            viewModel.actionType = viewModel.COOKING_TYPE
+        }
+
+        binding.eatingRadio.setOnClickListener {
+            viewModel.actionType = viewModel.EATING_TYPE
+        }
+
+        binding.otherRadio.setOnClickListener {
+            viewModel.actionType = viewModel.OTHER_TYPE
+        }
+
+        binding.timeToStartEdit.setOnClickListener { showDialogs() }
 
         return view
     }
@@ -27,7 +56,46 @@ class AddAction : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+    }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun showDialogs() {
+        val calendar = Calendar.getInstance()
+
+        DatePickerDialog(
+            requireContext(),
+            DatePickerDialog.OnDateSetListener { _, year, month, day ->
+                TimePickerDialog(
+                    requireContext(),
+                    TimePickerDialog.OnTimeSetListener { _, hour, minutes ->
+
+                        val dateFormat = SimpleDateFormat("yy-MM-dd HH:mm", Locale.ENGLISH)
+                        calendar.set(Calendar.YEAR, year)
+                        calendar.set(Calendar.MONTH, month)
+                        calendar.set(Calendar.DAY_OF_MONTH, day)
+                        calendar.set(Calendar.HOUR_OF_DAY, hour)
+                        calendar.set(Calendar.MINUTE, minutes)
+                        Log.d("asdasd", calendar.toString())
+                        binding
+                            .timeToStartEdit
+                            .setText(
+                                dateFormat
+                                    .format(
+                                        calendar.time
+                                    )
+                            )
+
+                    },
+                    calendar.get(Calendar.HOUR_OF_DAY),
+                    calendar.get(Calendar.MINUTE),
+                    true
+                ).show()
+
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
     }
 
 }
